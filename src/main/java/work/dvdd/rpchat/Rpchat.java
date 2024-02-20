fe8c3eb3382e130192f1f59694a3f48029e879eb
rinrien <117919623+rinrien@users.noreply.github.com>
2024-02-20 12:15:52 +0300

    Init

---
+++ src/main/java/work/dvdd/rpchat/Rpchat.java
@@ -0,0 +1,17 @@
+package work.dvdd.rpchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Rpchat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
